d6ea3aae4585edd1145ca93082a12db4f0427694
lsq <yan647@aliyun.com>
2018-02-06 10:56:49 +0800

    拖拽排序

--- app/controller/indexCtrl.js
+++ app/controller/indexCtrl.js
@@ -1,16 +1,19 @@
 module.exports = function(app) {
-    app.controller("touchCtrl",function($scope){
+    app.controller("indexCtrl",function($scope){
         $scope.init=function(){
             console.log('success');
-        }
+        };
         $scope.touchStart=function(){
             alert("touchStart");
-        }
+        };
         $scope.touchMove=function(){
             console.log("touchMove");
-        }
+        };
         $scope.touchEnd=function(){
             alert("touchEnd");
-        }
+        };
+
+        //拖拽移动
+        $scope.sortable = [{'name':'张春玲','age':28},{'name':'王晰','age':26},{'name':'吴正青','age':66}];
     });
-}
+};
--- app/main.js
+++ app/main.js
@@ -3,8 +3,10 @@ document.querySelector("#root").appendChild(greeter());
 
 var angular = require('angular');//引入angular
 require('ngtouch');
-var app = angular.module('myApp',["ngTouch"]);//定义一个angular模块
+//require('angular-ui-router');
+var app = angular.module('myApp',["ngTouch"/*,'angular-ui-router'*/]);//定义一个angular模块
 require('./directives/hello-world/hello-world.js')(app);//引入指令(directive)文件
+//require('./directives/angular-sortable-view/angular-sortable-view.js')(app);
 //require('./directives/batch-upload-files/batch-upload-files.js')(app);
-require('./controller/touchCtrl.js')(app);
+require('./controller/indexCtrl.js')(app);
 require('./css/style.css');//引入样式文件
