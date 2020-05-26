b0bb5387c5ce9636b4bd7ad14211e0396cbe2137
yan647 <yan647@aliyun.com>
2020-05-26 08:21:17 +0800

    improvement(eslint): 漏掉了eslintrc配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,10 @@ module.exports = {
         "es6": true
     },
     "extends": [
+        /*由于这行，所有在 规则页面 被标记为 “” 的规则将会默认开启。
+        可以在 npmjs.com 搜索 “eslint-config” 使用别人创建好的配置。
+        只有在配置文件中扩展了一个可分享的配置或者明确开启一个规则，
+        ESLint 才会去校验代码*/
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended"
@@ -21,5 +25,12 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        /*错误级别
+        off|0 关闭规则
+        warn|1 将规则视为一个警告（不会影响退出码）
+        error|2 将规则视为1个错误（退出码为1）
+        */
+        "semi":["error","always"],
+        "quotes":["error","double"]
     }
 };
