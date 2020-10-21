9b87dc0f1e7cd50432966ded2fe0665534d437cc
yan647 <yan647@aliyun.com>
2020-10-21 23:04:46 +0800

    improvement(leetcode): 每日一题

---
+++ leetcode/925.js
@@ -0,0 +1,25 @@
+/**
+ * Created by lsq on 2020/10/21.
+ */
+
+'use strict';
+
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function (name, typed) {
+  let a = 0, b = 0, n = name.length;
+  while (b < typed.length) {
+    if (a < n && (name[a] === typed[b])) {
+      b++;
+      a++;
+    } else if (b - 1 >= 0 && (typed[b] === typed[b - 1])) {
+      b++;
+    } else return false;
+  }
+  return a === n;
+};
+
+console.log(isLongPressedName("alex", "alexxr"));
