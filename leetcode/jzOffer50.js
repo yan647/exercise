a5c7b8e9c5aafcfa7e336f7fc7fd34496d0a75ce
yan647 <yan647@aliyun.com>
2020-11-16 09:50:19 +0800

    improvement(剑指Offer): 50题如果s为汉字的情况

--- leetcode/jzOffer50.js
+++ leetcode/jzOffer50.js
@@ -18,3 +18,23 @@ let firstUniqChar = function (s) {
 };
 
 console.log(firstUniqChar("abaccdeff"));
+
+
+let firstUniqChar1 = function (s) {
+  let map = new Map();
+  for (let a = 0; a < s.length; a++) {
+    let item = s[a];
+    if (map.has(item)) {
+      let count=map.get(item);
+      map.set(item, ++count);
+    } else map.set(item, 1);
+  }
+  for(let a=0;a<s.length;a++){
+    if(map.get(s[a])===1){
+      return s[a];
+    }
+  }
+  return " ";
+};
+
+console.log(firstUniqChar1("中国是中国人的"));
