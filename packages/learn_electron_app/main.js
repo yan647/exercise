be765b025f2f8c3ddccb15244625d6af7a75acdd
yan647 <yan647@aliyun.com>
2023-08-22 17:51:32 +0800

    feat: init

---
+++ packages/learn_electron_app/main.js
@@ -0,0 +1,40 @@
+// Empty
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  ipcMain.on('set-title', (event, title) => {
+    const webContents = event.sender;// todo
+    const win = BrowserWindow.fromBrowserView(webContents);// todo
+    win.setTitle(title);
+  });
+
+  mainWindow.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+console.log('hello world!');
---
+++ packages/learn_electron_app/preload.js
@@ -0,0 +1,19 @@
+// Empty
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  setTitle: (title) => ipcRenderer.send('set-title', title),
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) {
+      element.innerText = text;
+    }
+  };
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
