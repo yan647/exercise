98a914821a542de9e4fb7b17915e234f535d60e8
yan647 <yan647@aliyun.com>
2023-08-23 09:46:15 +0800

    feat: 解析

--- packages/type_challenge/00011-easy-tuple-to-object.ts
+++ packages/type_challenge/00011-easy-tuple-to-object.ts
@@ -3,8 +3,13 @@ const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
 type x=['tesla', 'model 3', 'model X', 'model Y']
 type y=typeof tuple;
 
-type TupleToObject<T>={
-
+/**
+ * 参考：
+ * https://juejin.cn/post/7086277203662471199
+ * https://juejin.cn/post/7265996663406968844#heading-11
+ * */
+type TupleToObject<T extends readonly any[]>={
+  [P in T[number]]:P
 }
 
 type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
