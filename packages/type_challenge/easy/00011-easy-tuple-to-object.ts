b22105e3f36405f2c23b2e635cff009e21dc8601
yan647 <yan647@aliyun.com>
2023-09-02 10:12:01 +0800

    feat: 添加思路

--- packages/type_challenge/easy/00011-easy-tuple-to-object.ts
+++ packages/type_challenge/easy/00011-easy-tuple-to-object.ts
@@ -7,6 +7,9 @@ type y=typeof tuple;
  * 参考：
  * https://juejin.cn/post/7086277203662471199
  * https://juejin.cn/post/7265996663406968844#heading-11
+ *
+ * T[number]的作用就是获取元组所有元素对应的类型，返回一个联合类型
+ * 因为使用了as const，所以得加readonly
  * */
 type TupleToObject<T extends readonly any[]>={
   [P in T[number]]:P
