d55d4190d78aef1a56415db4f6ba4fad38458283
yan647 <yan647@aliyun.com>
2024-05-22 09:09:38 +0800

    feat: 最简单的方法

---
+++ packages/learn_leetcode/2225.ts
@@ -0,0 +1,18 @@
+// 方法一，最麻烦的方法
+function findWinners1(matches: number[][]): number[][] {
+    let obj:Record<number,number>={}
+    let players=Array.from(new Set(matches.flat()))
+    players.forEach((item)=>obj[item]=0)
+    for(let i=0;i<matches.length;i++){
+        if(obj[matches[i][1]]){
+            obj[matches[i][1]]++
+        } else{
+            obj[matches[i][1]]=1
+        }
+    }
+    return [
+        Object.entries(obj).filter(([_key,value])=>Number(value)===0).map((item)=>Number(item[0])),
+        Object.entries(obj).filter(([_key,value])=>Number(value)===1).map((item)=>Number(item[0]))
+    ]
+};
+
