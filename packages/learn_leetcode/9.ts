dc9301fd1c19ff25f048dd9b71e8af85b830ffc6
yan647 <yan647@aliyun.com>
2025-04-13 22:08:02 +0800

    feat: 添加注释

--- packages/learn_leetcode/9.ts
+++ packages/learn_leetcode/9.ts
@@ -1,4 +1,19 @@
-const isPalindrome1 = function (x:number) {
+// 暴力解法
+function isPalindrome(x: number): boolean {
+    const list = String(x).split('')
+    const len = list.length;
+    for (let i = 0; i < list.length; i++) {
+        let item = list[i];
+        if (item !== list[len - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+};
+console.log(isPalindrome(1211));
+
+// 暴力解法
+const isPalindrome1 = function (x: number): boolean {
     let xStr = x.toString();
     let len = xStr.length;
     if (len <= 1) {
@@ -13,31 +28,13 @@ const isPalindrome1 = function (x:number) {
 };
 console.log(isPalindrome1(1122));
 
-//https://leetcode-cn.com/problems/palindrome-number/solution/hui-wen-shu-by-leetcode-solution/
-const isPalindrome2 = function (x:number) {
-    if (x < 0 || (x % 10 === 0 && x !== 0)) {
-        return false;
-    } else {
-        let a = 0;
-        while (x > a) {
-            a = a * 10 + x % 10;
-            x = Math.floor(x / 10);
-        }
-    }
-};
-console.log(isPalindrome2(1122));
+// 反转整个数字
+function isPalindrome2(x: number): boolean {
+    const str = String(x)
+    const list = str.split('')
+    const revertStr = list.reverse().join('')
+    return str === revertStr;
+}
 
+console.log(isPalindrome2(121));
 
-function isPalindrome(x: number): boolean {
-    const list=String(x).split('')
-    const len = list.length;
-    for (let i = 0; i < list.length; i++) {
-        let item = list[i];
-        if(item!==list[len-i-1]){
-            return false;
-        }
-    }
-    return true;
-};
-
-console.log(isPalindrome(1211));
